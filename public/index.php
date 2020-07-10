bc8207d39499a3d06e93a27be2cb88bcae0921eb
UnBMaster <unbmaster@outlook.com>
2020-07-10 09:47:32 -0300

    Update 11

--- public/index.php
+++ public/index.php
@@ -12,6 +12,10 @@
  * @version 0.1.0
  */
 
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+echo $actual_link;
+exit();
+
 
 ini_set('display_errors', true);
 require __DIR__ . '/../vendor/autoload.php';
