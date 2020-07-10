15c64cf6b30d32d837033b71b1ba0b5524440d16
UnBMaster <unbmaster@outlook.com>
2020-07-10 10:38:40 -0300

    Update 13

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
 /**
  * Ponto central das requisições e rotas de acesso aos serviços
  * @author UnBMaster <unbmaster@outlook.com>
- * @version 0.1.0
+ * @version 0.1.0 
  */
 
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
